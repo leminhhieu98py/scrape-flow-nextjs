100d598b5beb685033e27c5a04ab4685d5662021
leminhhieu98py <leminhhieu98py@gmail.com>
2025-02-18 22:58:36 +0700

    feat: implement the sidebar

---
+++ components/molecules/desktop-sidebar.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { CoinsIcon, HomeIcon, Layers2Icon, ShieldCheckIcon } from 'lucide-react';
+import Logo from './logo';
+import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
+import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
+
+const routes = [
+  {
+    href: '',
+    label: 'Home',
+    icon: HomeIcon
+  },
+  {
+    href: 'workflows',
+    label: 'Workflows',
+    icon: Layers2Icon
+  },
+  {
+    href: 'credentials',
+    label: 'Credentials',
+    icon: ShieldCheckIcon
+  },
+  {
+    href: 'billing',
+    label: 'Billing',
+    icon: CoinsIcon
+  }
+];
+
+function DesktopSidebar() {
+  const pathName = usePathname();
+  const activeRoute =
+    routes.find((route) => route.href.length > 0 && pathName.includes(route.href)) || routes[0];
+
+  return (
+    <div className="hidden relative md:block w-[280px] h-screen overflow-hidden bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate">
+      <div className="flex items-center justify-center gap-2 border-b-[1px] border-separate p-4">
+        <Logo />
+      </div>
+      <div className="flex flex-col p-2">
+        {routes.map((route, index) => (
+          <Link
+            key={index}
+            href={route.href}
+            className={cn(
+              '[&:not(:first-child)]:mt-2',
+              buttonVariants({
+                variant: route.href === activeRoute.href ? 'sidebarActiveItem' : 'sidebarItem'
+              })
+            )}
+          >
+            <route.icon size={20} />
+            {route.label}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default DesktopSidebar;
