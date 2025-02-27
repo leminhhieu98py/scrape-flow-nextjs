5c5fcc74f560ab416aa3fb9133d361a4a56b75e7
leminhhieu98py <leminhhieu98py@gmail.com>
2025-02-27 23:38:57 +0700

    feat: add login and signup for homepage

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,15 @@
+import { SignedOut, SignInButton, SignUpButton } from '@clerk/nextjs';
+
 function Homepage() {
-  return <div>Homepage Landing Page</div>;
+  return (
+    <>
+      <div>Homepage Landing Page</div>
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+    </>
+  );
 }
 
 export default Homepage;
