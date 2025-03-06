0c53174d6b81bc0cf6cfbe82d24152ff17277a20
leminhhieu98py <leminhhieu98py@gmail.com>
2025-03-06 22:49:21 +0700

    refactor: app-provider component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import AppProviders from '@/components/providers/AppProviders';
+import AppProviders from '@/components/templates/app-providers';
 import { ClerkProvider } from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ['latin'] });
