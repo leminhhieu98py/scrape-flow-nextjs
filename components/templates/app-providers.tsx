2dfa167fa82e79d3409d28d109c4a91a7563a6cc
leminhhieu98py <leminhhieu98py@gmail.com>
2025-03-10 22:20:24 +0700

    feat: config provider for the tanstack query

--- components/templates/app-providers.tsx
+++ components/templates/app-providers.tsx
@@ -2,12 +2,17 @@
 
 import { ThemeProvider } from 'next-themes';
 import { PropsWithChildren } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function AppProviders({ children }: PropsWithChildren) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
